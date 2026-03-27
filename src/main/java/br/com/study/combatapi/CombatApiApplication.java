f87ffe42acea52155ced0616037c3f2a664da753
xcorpiion <vinicius.da.silva.cruz.sanji@gmail.com>
2026-03-27 15:40:39 -0300

    Iniciando a api

---
+++ src/main/java/br/com/study/combatapi/CombatApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.study.combatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CombatApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CombatApiApplication.class, args);
+    }
+
+}
